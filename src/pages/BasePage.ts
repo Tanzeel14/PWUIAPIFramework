33fa5eee47e48242a007f7270551a5dc8b0c8eb2
Tanzeel <tanzeel1912@gmail.com>
2026-06-27 19:57:35 +0530

    updated Base page

--- src/pages/BasePage.ts
+++ src/pages/BasePage.ts
@@ -1,56 +1,71 @@
-import { Locator, Page } from "@playwright/test"
+import { Locator, Page } from "@playwright/test";
 
 export class BasePage {
 
-   protected readonly page:Page
-   
+    protected readonly page: Page;
+
+    //common locators across all pages:
+    protected readonly logo: Locator;
+    protected readonly searchBox: Locator;
+    protected readonly searchIcon: Locator;
+    protected readonly footerLinks: Locator;
+    protected readonly currency: Locator;
+    protected readonly cartButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.logo = page.getByAltText('naveenopencart');
+        this.searchBox = page.getByPlaceholder('Search');
+        this.searchIcon = page.locator('div#search button');
+        this.currency = page.locator('#form-currency');
+        this.footerLinks = page.locator('footer a');
+        this.cartButton = page.locator('div#cart button');
+    }
 
     //common locators/functionalities/actions
-
-    protected readonly logo:Locator;
-    protected readonly searchBox:Locator;
-    protected readonly searchIcon:Locator;
-    protected readonly footerLinks:Locator;
-    protected readonly currency:Locator;
-    protected readonly cartButton:Locator;
-
-     constructor(page:Page) {
-        this.page=page;
-        this.logo=page.getByAltText('naveenopencart')
-        this.searchIcon=page.locator('div#search button');
-        this.currency=page.locator('#form-currency');
-        this.footerLinks=page.locator('footer a');
-        this.searchBox=page.getByPlaceholder('Search')
-        
+    async isLogoVisible(): Promise<boolean> {
+        return this.logo.isVisible();
     }
 
-    async isLogoVisible():Promise<boolean>{
-        return await this.logo.isVisible();
-    }
-
-    async isSearchBoxVisible():Promise<boolean>{
+    async isSearchBoxVisible(): Promise<boolean> {
         return await this.searchBox.isVisible();
     }
 
-    async getPageFooterCount():Promise<number>{
+    async isCurrencyBoxVisible(): Promise<boolean> {
+        return await this.currency.isVisible();
+    }
+
+    async isCartButtonVisible(): Promise<boolean> {
+        return await this.cartButton.isVisible();
+    }
+
+    async getPageFootersCount(): Promise<number> {
         return await this.footerLinks.count();
     }
 
-    async getFooters():Promise<string[]>{
+    async getPageFooters(): Promise<string[]> {
         return await this.footerLinks.allInnerTexts();
     }
 
-    //page level generic methods
-    async getPageTitle():Promise<string> {
+    //page level generic methods:
+    async getPageTitle(): Promise<string> {
         return await this.page.title();
     }
 
-    getCurrentTitle():string{
+    getCurrentTitle(): string {
         return this.page.url();
     }
 
-    async waitForPageLoad(){
-        await this.page.waitForLoadState()
+    async waitForPageLoad() {
+        await this.page.waitForLoadState('load');
     }
+
+    async takeScreenshot(name: string) {
+        return await this.page.screenshot({
+            fullPage: true,
+            path: `reports/screenshot/${name}.png`
+        });
+    }
+
 
 }
