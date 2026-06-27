02b710875d416d5e416e2cce43e74878a4a81b95
Tanzeel <tanzeel1912@gmail.com>
2026-06-27 19:53:42 +0530

    updated yml file and product test file

--- tests/productpage.spec.ts
+++ tests/productpage.spec.ts
@@ -1,25 +1,40 @@
-import {test,expect} from '../src/fixtures/pagefixtures';
-import { HomePage } from '../src/pages/HomePage';
-import { Searchresult } from '../src/pages/Searchresults';
-import { csvUtil } from '../src/utils/csvutil';
+
+import { test, expect } from '../src/fixtures/pagefixtures';
 
 
-
-
-test.beforeEach(async({loginPage})=>
-{
+test.beforeEach(async ({ loginPage }) => {
     await loginPage.gotoLoginPage();
-    await loginPage.doLogin(process.env.USERNAME!,process.env.PASSWORD!)
-    //homePage= new HomePage(page)
-
+    await loginPage.doLogin(process.env.USERNAME!, process.env.PASSWORD!);
 });
 
-test('logo exists on product page',async({productInfoPage})=>{
 
-expect( await productInfoPage.isLogoVisible()).toBeTruthy();
-})
+test('comp logo exists on product page', async ({ basePage }) => {
+    expect(await basePage.isLogoVisible()).toBeTruthy();
+});
 
-test('verify product images count',async({HomePage}))
+test('footers exist on product page', async ({ basePage }) => {
+    expect(await basePage.getPageFooterCount()).toBe(16);
+});
+
+test('verify product images count', async ({ homePage, searchResultsPage, productInfoPage }) => {
+    await homePage.doSearch('macbook');
+    await searchResultsPage.selectProduct('MacBook Pro');
+    let imgCount = await productInfoPage.getProductImages();
+    console.log('total images: ', imgCount);
+    expect(imgCount).toBe(4);
+    //act vs exp
+});
 
 
-test('verify product information/Data', async({homePage}))
+test('verify product Information/Data', async ({ homePage, searchResultsPage, productInfoPage }) => {
+    await homePage.doSearch('macbook');
+    await searchResultsPage.selectProduct('MacBook Pro');
+    let actualProductInfoMap = await productInfoPage.getProductInfo();
+    console.log('Actual Product Details: ', actualProductInfoMap);
+    expect.soft(actualProductInfoMap.get('ProductHeader')).toBe('MacBook Pro');
+    expect.soft(actualProductInfoMap.get('Brand')).toBe('Apple');
+    expect.soft(actualProductInfoMap.get('Product Code')).toBe('Product 18');
+    expect.soft(actualProductInfoMap.get('Reward Points')).toBe('800');
+    expect.soft(actualProductInfoMap.get('ProductPrice')).toBe('$2,000.00');
+    expect.soft(actualProductInfoMap.get('ExTaxPrice')).toBe('$2,000.00');
+});
